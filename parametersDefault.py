118586065b2a9deed0a1db398de8b1325220e87e
lvalle <valleleonardo8@gmail.com>
2025-03-24 13:02:07 +0100

    initial

---
+++ parametersDefault.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+parametersUniform = {
+    "delta_E" : np.random.rand(),
+    "mu_E" : np.random.rand(),
+    "beta" : np.random.rand(),
+    "alpha" : np.random.rand(),
+    "delta_J" : np.random.rand(),
+    "mu_J" : np.random.rand(),
+    "omega" : np.random.rand(),
+    "mu_M" : np.random.rand(),
+    "a" : np.random.rand(),
+    "b_M" : np.random.rand(),
+    "alpha_M" : np.random.rand(),
+    "Lambda" : np.random.rand(),
+    "b_H" : np.random.rand(),
+    "mu_H" : np.random.rand(),
+    "alpha_H" : np.random.rand(),
+    "gamma_H" : np.random.rand()
+}
+
+parameters = {
+    "delta_E" : 0.6,
+    "mu_E" : 0.875,
+    "beta" : np.random.rand(),
+    "alpha" : np.random.rand(),
+    "delta_J" : 0.09,
+    "mu_J" : np.random.rand(),
+    "omega" : 0.5,
+    "mu_M" : np.random.rand(),
+    "a" : 0.2,
+    "b_M" : 0.9,
+    "alpha_M" : 1/30,
+    "Lambda" : 12, # test
+    "b_H" : 0.8,
+    "mu_H" : 0.001, # test
+    "alpha_H" : 0.4,
+    "gamma_H" : 1/5.5
+}
+
+initialConditions ={
+    "eggs":10000,
+    "juveniles":10000,
+    "susceptible_m":50000,
+    "exposed_m":10000,
+    "infected_m":10000,
+    "susceptible_h":10000,
+    "exposed_h":1000,
+    "infected_h":1000,
+    "recovered_h":0
+}
+
+initialConditionsRandom = (0,100*np.random.rand(10,1).reshape(10,-1))
