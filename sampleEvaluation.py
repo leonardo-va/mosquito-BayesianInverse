902705ade07d686e62aa60f24e274e53c1f586ec
lvalle <valleleonardo8@gmail.com>
2025-04-23 12:54:53 +0200

    plot sample evaluation

--- sampleEvaluation.py
+++ sampleEvaluation.py
@@ -1,10 +1,19 @@
 from pandas import read_csv
+from matplotlib import pyplot as plt
+import numpy as np
 
-samplesCSVPath = "/home/leo/src/mosquito-BayesianInverse/samples_reduced_4000_large_noise.csv"
+samplesCSVPath = "samples_reduced_4000_smallnoise_moredate.csv"
 samplesDF = read_csv(samplesCSVPath)
-print(samplesDF.keys())
+print(samplesDF)
 
 samplingResultArray = samplesDF.to_numpy()
 samplesAlpha = samplesDF["p4"].to_numpy()
 samplesMu = samplesDF["p8"].to_numpy()
-print(samplesMu)
+
+print(f'mean alpha: {np.mean(samplesAlpha)}, mean mu: {samplesMu}')
+plt.hist(samplesAlpha,50)
+plt.axvline(10**(-6), color='r')
+plt.show()
+plt.hist(samplesMu, 50)
+plt.axvline(0.1,color='r')
+plt.show()
