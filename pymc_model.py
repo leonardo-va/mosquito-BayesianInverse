b81f714a09077ff914f3ac858ad90eca5da42a5b
lvalle <valleleonardo8@gmail.com>
2025-05-15 10:06:54 +0200

    pymc script update

--- pymc_model.py
+++ pymc_model.py
@@ -8,13 +8,33 @@ import quantityOfInterest
 
 
 parameters = parametersDefault.defaultParameters
+quantitiesOfInterest = [quantityOfInterest.numberOfMosquitos, quantityOfInterest.numberOfEggs]
 ode_Data = generateData(100, [0,10], parameters, parametersDefault.defaultInitialConditions, 
-                        [quantityOfInterest.numberOfMosquitos, 
-                         quantityOfInterest.numberOfEggs])
+                        quantitiesOfInterest)
 print("---------------------------------------------------------")
-odeModel = pm.ode.DifferentialEquation(lambda u,t,p: mosquitoModel(t,u,p).tolist(), ode_Data["ts"], n_states=9, n_theta=16)
-pm.TruncatedNormal()
+odeModel = pm.ode.DifferentialEquation(lambda u,t,p: mosquitoModel(t,u,p).tolist(),
+                                        ode_Data["ts"], n_states=9, n_theta=16)
+
 with pm.Model() as model:
     #Priors
     alpha = pm.TruncatedNormal("alpha", mu = parameters["alpha"], sigma = 0.5)
-    mu_M = pm.TruncatedNormal("alpha", mu = parameters["mu_M"], sigma = 0.5)
+    mu_M = pm.TruncatedNormal("mu_M", mu = parameters["mu_M"], sigma = 0.5)
+    parameters["alpha"] = alpha
+    parameters["mu_M"] = mu_M
+    #Forward
+    odeSolution = odeModel(list(parametersDefault.defaultInitialConditions.values()), list(parameters.values()))
+    print("***********************", odeSolution)
+    forwardSolution = []
+    for qoi in quantitiesOfInterest:
+        forwardSolution.append(qoi(odeSolution[0]))
+    #Likelihood
+    observation = []
+    for qoi in quantitiesOfInterest:
+        observation.append(qoi(ode_Data["y"]))
+    pm.Normal("Y_obs", mu=odeSolution, sigma=5000, observed = ode_Data["y"])
+    print("hi")
+
+sampler = "NUTS PyMC ODE"
+tune = draws = 15
+with model:
+    trace_pymc_ode = pm.sample(tune=tune, draws=draws)
