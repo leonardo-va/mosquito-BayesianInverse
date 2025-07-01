d07d0d0b9ec5d9193ba0c3a7873d13d64381b9f4
lvalle <valleleonardo8@gmail.com>
2025-07-01 11:27:32 +0200

    fix model code translation and save samples

--- src/runSampler.py
+++ src/runSampler.py
@@ -11,10 +11,11 @@ def sample(stan_code:str, data:dict, num_samples:int):
     return df
 
 def save_samples(samples_dataframe, folder_path, setup:dict = None):
+    print("************", folder_path)
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
-    samples_filename = f"samples/samples_{t.time()}"
-    samples_dataframe.to_csv(f"{samples_filename}.csv")
+    samples_filename = f"samples_{t.time()}"
+    samples_dataframe.to_csv(os.path.join(folder_path, f"{samples_filename}.csv"))
     if(setup is not None):
-        with open(f"{samples_filename}_setup.json", "w") as f:
+        with open(os.path.join(folder_path, f"{samples_filename}_setup.json"), "w") as f:
             json.dump(setup, f, indent=4)
